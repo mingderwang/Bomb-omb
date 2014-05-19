ac8cf5c91e950e5a9048bc0ebf85c772aa650b6c
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-05-19 16:33:07 +1000

    add collide basic and fix scaleFactor

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 (function(){
 
   var resolutionGame = navigator.isCocoonJS ? [window.innerWidth, window.innerHeight] : [480, 320],
-      scaleFactor = [Math.round(window.innerWidth / 480 * 10) / 10, Math.round(window.innerHeight / 320 * 10) / 10];
-
-      console.log(scaleFactor[0], scaleFactor[1]);
+      scaleFactor = [Math.round(resolutionGame[0] / 480 * 10) / 10, Math.round(resolutionGame[1] / 320 * 10) / 10];
 
   var BootState = {
    create: function(){
       if(!navigator.isCocoonJS){
-        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //resize your window to see the stage resize too
-        this.game.scale.setShowAll();
-        this.game.scale.refresh();
-        this.game.scale.setScreenSize(true);
+        // this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //resize your window to see the stage resize too
+        // this.game.scale.setShowAll();
+        // this.game.scale.refresh();
+        // this.game.scale.setScreenSize(true);
       }else{
         this.world.scale.setTo(scaleFactor[0], scaleFactor[1]);
       }
@@ -24,6 +22,8 @@
     preload: function(){
       this.load.image('background', 'assets/bg.png');
       this.load.image('bomb', 'assets/bomb.png');
+      this.load.image('containerLeft', 'assets/containerLeft.jpg');
+      this.load.image('containerRight', 'assets/containerRight.jpg');
     },
 
     create: function(){
@@ -33,12 +33,22 @@
 
   var GameState = {
     create: function(){
+      // this.physics.startSystem();
+
       this.bg = this.add.image(0, 0, 'background');
+
+      this.groupContainer = this.add.group();
+      this.groupContainer.enableBody = true;
+      this.groupContainer.physicsBodyType = Phaser.Physics.ARCADE;
+
+      this.containerLeft = this.groupContainer.create(0, 89, 'containerLeft');
+      this.containerRight = this.groupContainer.create(357, 89, 'containerRight');
 
       this.bomb = this.add.sprite(resolutionGame[0] / 2, -32, 'bomb');
       this.bomb.inputEnabled = true;
-      this.bomb.dragged = false;
       this.bomb.anchor.setTo(0.5,0.5);
+      this.bomb.dragged = false;
+      this.bomb.collideAllowed = false;
 
       this.physics.enable(this.bomb, Phaser.Physics.ARCADE);
       this.bomb.enableBody = true;
@@ -53,29 +63,54 @@
 
     update: function(){
       // console.log(this.input.activePointer.x, this.input.activePointer.y);
+
+      game.physics.arcade.overlap(this.bomb, this.groupContainer, this.collideContainer, null, this);
+
       if(this.bomb.dragged){
-        if(navigator.isCocoonJS){
-          this.bomb.x = this.input.activePointer.x / scaleFactor[0];
-          this.bomb.y = this.input.activePointer.y / scaleFactor[1];
-        }else{
-          this.bomb.x = this.input.activePointer.x;
-          this.bomb.y = this.input.activePointer.y;
-        }
+        this.bomb.x = this.input.activePointer.x / scaleFactor[0];
+        this.bomb.y = this.input.activePointer.y / scaleFactor[1];
       }
+
+      if(this.bomb.y > resolutionGame[1] - 16){
+        this.physics.arcade.moveToXY(this.bomb, Math.floor(Math.random() * ( 360 - 130 ) + 130), Math.floor(Math.random() * ( resolutionGame[1] - 130 ) + 130 ));
+      }
+      if(this.bomb.y < 100 + 16){
+        this.physics.arcade.moveToXY(this.bomb, Math.floor(Math.random() * ( 360 - 130 ) + 130), Math.floor(Math.random() * ( resolutionGame[1] - 130 ) + 130 ));
+      }
+
+      console.log(this.bomb.collideAllowed);
+    },
+
+    render: function(){
+      // game.debug.spriteBounds(this.bomb);
+      // game.debug.spriteBounds(this.containerLeft);
+      // game.debug.spriteBounds(this.containerRight);
     },
 
     dragBomb: function(bomb, pointer){
       this.bomb.dragged = true;
+      this.bomb.collideAllowed = true;
     },
 
     dropBomb: function(bomb, pointer){
+      if(Phaser.Rectangle.contains(this.containerLeft.getBounds(), this.bomb.x + this.bomb.width / 2, this.bomb.y + this.bomb.height / 2) ||
+        Phaser.Rectangle.contains(this.containerRight.getBounds(), this.bomb.x + this.bomb.width / 2, this.bomb.y + this.bomb.height / 2)){
+        this.bomb.collideAllowed = true;
+      }
       this.bomb.dragged = false;
+      // this.bomb.collideAllowed = false;
     },
 
     createTween: function(objectToTween){
       objectToTween.tween = this.add.tween(objectToTween);
       objectToTween.tween.to({ y: Math.random() * resolutionGame[0] }, 5000, Phaser.Easing.Linear.None);
       objectToTween.tween.start();
+    },
+
+    collideContainer: function(bomb, container){
+      if(!bomb.collideAllowed){
+        this.physics.arcade.moveToXY(bomb, Math.floor(Math.random() * ( 360 - 130 ) + 130), Math.floor(Math.random() * ( resolutionGame[1] - 130 ) + 130 ));
+      }
     }
   };
 
