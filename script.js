902fc5270e90ce1c323a443ab10ad4a9ed5244ab
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-05-19 15:31:59 +1000

    generate a bomb moving a random point

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 (function(){
 
   var resolutionGame = navigator.isCocoonJS ? [window.innerWidth, window.innerHeight] : [480, 320],
-      scaleFactor = [window.innerWidth / 480, window.innerHeight / 320];
+      scaleFactor = [Math.round(window.innerWidth / 480 * 10) / 10, Math.round(window.innerHeight / 320 * 10) / 10];
+
+      console.log(scaleFactor[0], scaleFactor[1]);
 
   var BootState = {
    create: function(){
@@ -11,8 +13,7 @@
         this.game.scale.refresh();
         this.game.scale.setScreenSize(true);
       }else{
-        console.log(window.innerWidth / 480, window.innerHeight / 320);
-        this.world.scale.setTo(window.innerWidth / 480, window.innerHeight / 320);
+        this.world.scale.setTo(scaleFactor[0], scaleFactor[1]);
       }
 
       this.state.start('preload');
@@ -34,16 +35,24 @@
     create: function(){
       this.bg = this.add.image(0, 0, 'background');
 
-      this.bomb = this.add.sprite(100, 100, 'bomb');
+      this.bomb = this.add.sprite(resolutionGame[0] / 2, -32, 'bomb');
       this.bomb.inputEnabled = true;
       this.bomb.dragged = false;
       this.bomb.anchor.setTo(0.5,0.5);
+
+      this.physics.enable(this.bomb, Phaser.Physics.ARCADE);
+      this.bomb.enableBody = true;
+      this.bomb.physicsBodyType = Phaser.Physics.ARCADE;
+
+      //need to make this change in fonction of the different scale
+      this.physics.arcade.moveToXY(this.bomb, Math.floor(Math.random() * ( 360 - 130 ) + 130), Math.floor(Math.random() * ( resolutionGame[1] - 130 ) + 130 ));
 
       this.bomb.events.onInputDown.add(this.dragBomb, this);
       this.bomb.events.onInputUp.add(this.dropBomb, this);
     },
 
     update: function(){
+      // console.log(this.input.activePointer.x, this.input.activePointer.y);
       if(this.bomb.dragged){
         if(navigator.isCocoonJS){
           this.bomb.x = this.input.activePointer.x / scaleFactor[0];
@@ -57,12 +66,16 @@
 
     dragBomb: function(bomb, pointer){
       this.bomb.dragged = true;
-      // console.log('drag', Math.floor(bomb.x), Math.floor(bomb.y), Math.floor(pointer.x), Math.floor(pointer.y));
     },
 
     dropBomb: function(bomb, pointer){
       this.bomb.dragged = false;
-      // console.log('drop', Math.floor(bomb.x), Math.floor(bomb.y), Math.floor(pointer.x), Math.floor(pointer.y));
+    },
+
+    createTween: function(objectToTween){
+      objectToTween.tween = this.add.tween(objectToTween);
+      objectToTween.tween.to({ y: Math.random() * resolutionGame[0] }, 5000, Phaser.Easing.Linear.None);
+      objectToTween.tween.start();
     }
   };
 
